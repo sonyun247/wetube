5200c31ec8879147eda5b73d95d448b6bdbbca99
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2020-01-03 15:53:05 +0900

    babel

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
 
+const handleHome = (req, res) => res.send("home");
+
+const handleTest = (req, res) => res.send("test");
+
+app.get("/", handleHome);
+app.get("/test", handleTest);
 app.listen(PORT, handleListening);
