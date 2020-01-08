cab618f3d4d253e4835ba3d72a74b7ac3a33e7d1
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2020-01-09 06:48:06 +0900

    webpack
    mini-css-extract-plugin
    -autoprefixer
    -css/postcss/sass loader
    -babel loader / polyfill

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  mode: MODE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js"
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "styles.css" })],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
